5e2296b70972f54901f0767db42a7a9c1fabfbcd
dkagh012 <qowjdxo00@gmail.com>
2023-03-09 19:16:13 +0900

    first commit

---
+++ 1008.js
@@ -0,0 +1,5 @@
+// var input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+var input = require('fs').readFileSync('example.txt').toString().split('\n');
+a = input[0];
+b = input[1];
+console.log(a / b);
