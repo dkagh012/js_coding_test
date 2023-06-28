dd54bbf59d68a1616c9f920978550543c5d630eb
dkagh012 <qowjdxo00@gmail.com>
2023-06-29 00:13:00 +0900

    해설

--- 1010.js
+++ 1010.js
@@ -19,6 +19,8 @@ function solution(inputArr) {
     // memo 배열은 이전 계산 결과를 저장하기 위한 용도로 사용될 것입니다. 배열의 크기는 30x30으로 설정되어 있습니다.
     for (let i = 1; i <= n; i++) {
       for (let j = i; j <= m; j++) {
+        // 이중 반복문을 사용하여 memo 배열을 채웁니다. 
+        // 외부 반복문은 i가 1부터 n까지 증가하면서 반복하고, 내부 반복문은 j가 i부터 m까지 증가하면서 반복합니다.
         if (j === i) {
           memo[i][j] = 1;
         } else if (i === 1) {
@@ -30,8 +32,12 @@ function solution(inputArr) {
           }
           memo[i][j] = temp;
         }
+        // 위의 두 조건문에 해당하지 않는 경우, 즉 일반적인 경우에는 temp라는 변수를 선언하고 0으로 초기화합니다. 
+        // 그리고 다시 한 번 반복문을 사용하여 k가 1부터 j보다 작은 동안 반복하면서 temp에 이전 계산 결과인 memo[i-1][k]를 더해줍니다. 
+        // 이는 k를 마지막으로 하는 i-1자리 수를 만들 수 있는 경우의 수를 모두 더한 것입니다.
       }
     }
+    // 내부 반복문이 종료되면 memo[i][j]에 temp 값을 할당합니다.
     answer.push(memo[n][m]);
   });
   // console.log(answer.join("\n"));
