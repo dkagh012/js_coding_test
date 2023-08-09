55ffec3490e8e248e1ab6dbfe162ca1f315030e5
dkagh012 <qowjdxo00@gmail.com>
2023-08-09 19:22:55 +0900

    1145 마무리

--- 1145.js
+++ 1145.js
@@ -3,14 +3,25 @@ var input = require("fs").readFileSync("example.txt").toString().split("\n");
 // let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 let inputArr = [];
+let cnt = 0;
+let sum = 1;
 for (let i = 0; i < input.length; i++) {
   inputArr.push(input[i].split(" "));
 }
-console.log(inputArr[0].length);
-for (let i = 0; i < 100; i++) {
-  for (let j = 0; j < inputArr[0].length; j++) {}
+
+while (true) {
+  cnt = 0;
+
+  for (let i = 0; i < inputArr[0].length; i++) {
+    if (sum % inputArr[0][i] === 0) cnt++
+  }
+
+  if (cnt >= 3) { break; }
+
+  sum++;
 }
 
+console.log(sum);
 /*
 
 다섯 개의 자연수가 있다. 
